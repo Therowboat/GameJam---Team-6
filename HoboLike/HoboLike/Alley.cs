937f99ee3b067478c705e1f8825cc89670a37c40
Lauge Møller Andersen <46719124+LaugeM@users.noreply.github.com>
2025-10-20 13:02:53 +0200

    Created room abstract class and room children

---
+++ HoboLike/HoboLike/Alley.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoboLike
+{
+    public class Alley : Room
+    {
+        public string name = "Alley";
+    }
+}
---
+++ HoboLike/HoboLike/Underpass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoboLike
+{
+    public class Underpass : Room
+    {
+        public string name = "Underpass";
+    }
+}
