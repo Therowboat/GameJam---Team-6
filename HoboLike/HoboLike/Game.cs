0996c37a0edeecd17f1e68eec93ae553cecb2fb4
Marcus will <marcuswill5974@gmail.com>
2025-10-20 13:04:04 +0200

    Player Class and Game Class added

---
+++ HoboLike/HoboLike/Game.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoboLike
+{
+    public class Game
+    {
+        public Player player;
+
+        public void Start () 
+        {
+            bool isrunning = true;
+            while (isrunning) 
+            {
+               
+                var input = Console.ReadKey();
+
+                switch (input.Key) 
+                {
+                    case ConsoleKey.Q:
+                        Console.Clear();
+                        Console.WriteLine("Case 1");
+                            break;
+                    case ConsoleKey.W:
+                        
+                        break;
+                    case ConsoleKey.E:
+                        
+                        break;
+                    case ConsoleKey.R:
+                        isrunning = false;
+                        break;
+
+                }
+            }
+        }
+    }
+}
